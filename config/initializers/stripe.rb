6e43be9cb3f1929a4f1f08c714e5e497477e1408
dth2k1 <gblhiep@gmail.com>
2023-03-02 16:49:45 +0700

    init

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,5 @@
+Rails.configuration.stipe = {
+  :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+  :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
+}
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
